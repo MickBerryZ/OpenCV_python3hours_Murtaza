f1c74503cd3ec6f8996d9c58effd5190a1025de4
Thawatchai Sangthep <94940261+MickBerryZ@users.noreply.github.com>
2022-12-08 15:19:54 +0700

    first commit

---
+++ chapter1.1_HowtoImportVDO.py
@@ -0,0 +1,3 @@
+import cv2
+
+cap = cv2.VideoCapture("Resourece/jaypark_test.mp4")
---
+++ chapter1_HowtoImportPicture.py
@@ -0,0 +1,7 @@
+import cv2
+print("Package Imported")
+
+img = cv2.imread("Resources/torres.jpeg")
+
+cv2.imshow("output",img)
+cv2.waitKey(5000) # if set 1000, that means 1 second
