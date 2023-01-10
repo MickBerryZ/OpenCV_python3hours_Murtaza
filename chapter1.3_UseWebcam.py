5a635dfe1e026d967f2665fa2b1aad6bfadf87fe
Thawatchai Sangthep <94940261+MickBerryZ@users.noreply.github.com>
2023-01-10 16:25:10 +0700

    VirturalPrint

--- chapter1.3_UseWebcam.py
+++ chapter1.3_UseWebcam.py
@@ -1,13 +1,16 @@
 import cv2
 
+frameWidth = 640
+frameHeight = 480
+
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-cap.set(10, 100)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Video", img)
+    cv2.imshow("Result", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
